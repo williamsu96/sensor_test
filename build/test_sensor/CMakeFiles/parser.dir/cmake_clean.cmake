36ae7657734dc75176e7e48769d27c43c16936a7
William Su <williamsu96@gmail.com>
2018-11-01 15:13:35 -0500

    basic urdf

---
+++ build/test_sensor/CMakeFiles/parser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/parser.dir/src/parser.cpp.o"
+  "/home/williamsu/sensor_test/devel/lib/test_sensor/parser.pdb"
+  "/home/williamsu/sensor_test/devel/lib/test_sensor/parser"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
